4cce91ede8c535307204d3f15b918896205045bf
John Chuma <johnchuma@Johns-MacBook-Air.local>
2021-07-13 07:39:27 -0400

    Edited filter dropdown menus

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -7,6 +7,28 @@ import {BsViewStacked} from 'react-icons/bs';
 import {IoIosArrowDown} from 'react-icons/io';
 
 function Filters() {
+
+  const handleStateClick = () => {
+    const box = document.getElementById('state-list');
+    const newLink = document.getElementById('new');
+    if (box.style.display === 'flex') {
+      box.style.display = 'none';
+      newLink.style.marginLeft = 0;
+    } else {
+      box.style.display = 'flex';
+      newLink.style.marginLeft = '-77px';
+    }
+  }
+
+  const handleCountryClick = () => {
+    const box = document.getElementById('country-list');
+    if (box.style.display === 'flex') {
+      box.style.display = 'none';
+    } else {
+      box.style.display = 'flex';
+    }
+  }
+
   return (
     <div className="Filters">
       <div className='left'>
@@ -14,103 +36,105 @@ function Filters() {
           <FaFire />
          <p>Hot</p>
         </div>
-        <div className='country-container'>
-          <select className='country' defaultValue='United States'>
-            <option value='Everywhere'>Everywhere</option>
-            <option value='United States' selected>United States</option>
-            <option value='Argentina'>Argentina</option>
-            <option value='Australia'>Australia</option>
-            <option value='Bulgaria'>Bulgaria</option>
-            <option value='Canada'>Canada</option>
-            <option value='Chile'>Chile</option>
-            <option value='Colombia'>Colombia</option>
-            <option value='Croatia'>Croatia</option>
-            <option value='Czech Republic'>Czech Republic</option>
-            <option value='Finland'>Finland</option>
-            <option value='France'>France</option>
-            <option value='Germany'>Germany</option>
-            <option value='Greece'>Greece</option>
-            <option value='Hungary'>Hungary</option>
-            <option value='Iceland'>Iceland</option>
-            <option value='India'>India</option>
-            <option value='Ireland'>Ireland</option>
-            <option value='Italy'>Italy</option>
-            <option value='Japan'>Japan</option>
-            <option value='Malaysia'>Malaysia</option>
-            <option value='Mexico'>Mexico</option>
-            <option value='New Zealand'>New Zealand</option>
-            <option value='Philippines'>Philippines</option>
-            <option value='Poland'>Poland</option>
-            <option value='Portugal'>Portugal</option>
-            <option value='Puerto Rico'>Puerto Rico</option>
-            <option value='Romania'>Romania</option>
-            <option value='Serbia'>Serbia</option>
-            <option value='Singapore'>Singapore</option>
-            <option value='Spain'>Spain</option>
-            <option value='Sweden'>Sweden</option>
-            <option value='Taiwan'>Taiwan</option>
-            <option value='Thailand'>Thailand</option>
-            <option value='Turkey'>Turkey</option>
-            <option value='United Kingdom'>United Kingdom</option>
-          </select>
+        <div className='country-menu'>
+          <button className='country-menu-button-visible' onClick={handleCountryClick}>United States <IoIosArrowDown /></button>
+          <div className='country-list' id='country-list'>
+            <button>Everywhere</button>
+            <button id='united-states'>United States</button>
+            <button>Argentina</button>
+            <button>Australia</button>
+            <button>Bulgaria</button>
+            <button>Canada</button>
+            <button>Chile</button>
+            <button>Colombia</button>
+            <button>Croatia</button>
+            <button>Czech Republic</button>
+            <button>Finland</button>
+            <button>France</button>
+            <button>Germany</button>
+            <button>Greece</button>
+            <button>Hungary</button>
+            <button>Iceland</button>
+            <button>India</button>
+            <button>Ireland</button>
+            <button>Italy</button>
+            <button>Japan</button>
+            <button>Malaysia</button>
+            <button>Mexico</button>
+            <button>New Zealand</button>
+            <button>Philippines</button>
+            <button>Poland</button>
+            <button>Portugal</button>
+            <button>Puerto Rico</button>
+            <button>Romania</button>
+            <button>Serbia</button>
+            <button>Singapore</button>
+            <button>Spain</button>
+            <button>Sweden</button>
+            <button>Taiwan</button>
+            <button>Thailand</button>
+            <button>Turkey</button>
+            <button>United Kingdom</button>
+          </div>
         </div>
-        <div className='state-container'>
-          <select className='state' defaultValue='All'>
-            <option value='All' selected>All</option>
-            <option value='Alaska'>Alaska</option>
-            <option value='Alabama'>Alabama</option>
-            <option value='Arkansas'>Arkansas</option>
-            <option value='Arizona'>Arizona</option>
-            <option value='California'>California</option>
-            <option value='Colorado'>Colorado</option>
-            <option value='Connecticut'>Connecticut</option>
-            <option value='District of Colombia'>District of Colombia</option>
-            <option value='Delaware'>Delaware</option>
-            <option value='Florida'>Florida</option>
-            <option value='Georgia'>Georgia</option>
-            <option value='Hawaii'>Hawaii</option>
-            <option value='Iowa'>Iowa</option>
-            <option value='Idaho'>Idaho</option>
-            <option value='Illinois'>Illinois</option>
-            <option value='Indiana'>Indiana</option>
-            <option value='Kansas'>Kansas</option>
-            <option value='Kentucky'>Kentucky</option>
-            <option value='Louisiana'>Louisiana</option>
-            <option value='MA'>Massachusetts</option>
-            <option value='Maryland'>Maryland</option>
-            <option value='Maine'>Maine</option>
-            <option value='Michigan'>Michigan</option>
-            <option value='Minnesota'>Minnesota</option>
-            <option value='Missouri'>Missouri</option>
-            <option value='Mississippi'>Mississippi</option>
-            <option value='Montana'>Montana</option>
-            <option value='North Carolina'>North Carolina</option>
-            <option value='North Dakota'>North Dakota</option>
-            <option value='Nebraska'>Nebraska</option>
-            <option value='New Hampshire'>New Hampshire</option>
-            <option value='New Jersey'>New Jersey</option>
-            <option value='New Mexico'>New Mexico</option>
-            <option value='Nevada'>Nevada</option>
-            <option value='New York'>New York</option>
-            <option value='Ohio'>Ohio</option>
-            <option value='Oklahoma'>Oklahoma</option>
-            <option value='Oregon'>Oregon</option>
-            <option value='Pennsylvania'>Pennsylvania</option>
-            <option value='Rhode Island'>Rhode Island</option>
-            <option value='South Carolina'>South Carolina</option>
-            <option value='South Dakota'>South Dakota</option>
-            <option value='Tennessee'>Tennessee</option>
-            <option value='Texas'>Texas</option>
-            <option value='Utah'>Utah</option>
-            <option value='Virginia'>Virginia</option>
-            <option value='Vermont'>Vermont</option>
-            <option value='Washington'>Washington</option>
-            <option value='Wisconsin'>Wisconsin</option>
-            <option value='West Virginia'>West Virginia</option>
-            <option value='Wyoming'>Wyoming</option>
-          </select>
+        <div className='state-menu'>
+          <button className='state-menu-button-visible' onClick={handleStateClick}>All <IoIosArrowDown /></button>
+          <div className='state-list' id='state-list'>
+            <button id='all'>All</button>
+            <button>Alaska</button>
+            <button>Alabama</button>
+            <button>Arkansas</button>
+            <button>Arizona</button>
+            <button>California</button>
+            <button>Colorado</button>
+            <button>Connecticut</button>
+            <button>District of Colombia</button>
+            <button>Delaware</button>
+            <button>Florida</button>
+            <button>Georgia</button>
+            <button>Hawaii</button>
+            <button>Iowa</button>
+            <button>Idaho</button>
+            <button>Illinois</button>
+            <button>Indiana</button>
+            <button>Kansas</button>
+            <button>Kentucky</button>
+            <button>Louisiana</button>
+            <button>Massachusetts</button>
+            <button>Maryland</button>
+            <button>Maine</button>
+            <button>Michigan</button>
+            <button>Minnesota</button>
+            <button>Missouri</button>
+            <button>Mississippi</button>
+            <button>Montana</button>
+            <button>North Carolina</button>
+            <button>North Dakota</button>
+            <button>Nebraska</button>
+            <button>New Hampshire</button>
+            <button>New Jersey</button>
+            <button>New Mexico</button>
+            <button>Nevada</button>
+            <button>New York</button>
+            <button>Ohio</button>
+            <button>Oklahoma</button>
+            <button>Oregon</button>
+            <button>Pennsylvania</button>
+            <button>Rhode Island</button>
+            <button>South Carolina</button>
+            <button>South Dakota</button>
+            <button>Tennessee</button>
+            <button>Texas</button>
+            <button>Utah</button>
+            <button>Virginia</button>
+            <button>Vermont</button>
+            <button>Washington</button>
+            <button>Wisconsin</button>
+            <button>West Virginia</button>
+            <button>Wyoming</button>
+          </div>
         </div>
-        <div className='new'>
+        <div className='new' id='new'>
           <BsGearWide />
           <p>New</p>
         </div>
