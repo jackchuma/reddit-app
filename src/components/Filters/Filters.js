c22f121330f46cd3a045ed1e2e0128663c1ef818
John Chuma <johnchuma@Johns-MacBook-Air.local>
2021-07-14 21:29:54 -0400

    Filters dropdown functionality added

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Filters.css';
 import {FaFire} from 'react-icons/fa';
 import {BsGearWide} from 'react-icons/bs';
 import {AiOutlineArrowUp} from 'react-icons/ai';
-import {BsViewStacked} from 'react-icons/bs';
+import {BsViewStacked, BsArrowUpRight} from 'react-icons/bs';
 import {IoIosArrowDown} from 'react-icons/io';
 import {stateList} from './states';
 import {countryList} from './countries';
 
 function Filters() {
 
+  const [selectedCountry, setSelectedCountry] = useState({name: 'United States', id: 'united-states'});
+  const [selectedState, setSelectedState] = useState({name: 'All', id: 'all'});
+
   const handleStateClick = () => {
     const box = document.getElementById('state-list');
-    const newLink = document.getElementById('new');
+
     if (box.style.display === 'flex') {
       box.style.display = 'none';
-      newLink.style.marginLeft = 0;
     } else {
       box.style.display = 'flex';
-      newLink.style.marginLeft = '-77px';
     }
-  }
+  };
+
+  const handleStateListClick = (e) => {
+    const oldLink = document.getElementById(selectedState.id);
+    oldLink.style.color = '#878a8c';
+    setSelectedState({name: e.target.value, id: e.target.id});
+
+    const box = document.getElementById('state-list');
+    const newLink = document.getElementById('new');
+    const selected = document.getElementById(e.target.id);
+    
+    box.style.display = 'none';
+    newLink.style.marginLeft = 0;
+
+    selected.style.color = '#0079d3';
+  };
 
   const handleCountryClick = () => {
     const box = document.getElementById('country-list');
@@ -29,7 +45,44 @@ function Filters() {
     } else {
       box.style.display = 'flex';
     }
-  }
+  };
+
+  const handleCountryListClick = (e) => {
+    const oldLink = document.getElementById(selectedCountry.id);
+    oldLink.style.color = '#878a8c';
+    setSelectedCountry({name: e.target.value, id: e.target.id});
+
+    const box = document.getElementById('country-list');
+    const selected = document.getElementById(e.target.id);
+    box.style.display = 'none';
+    selected.style.color = '#0079d3';
+
+    const stateMenu = document.getElementById('state-menu');
+
+    if (e.target.value === 'United States') {
+      stateMenu.style.display = 'flex';
+    } else {
+      stateMenu.style.display = 'none';
+    }
+  };
+
+  const handleDotsClick = () => {
+    const box = document.getElementById('dots-dropdown');
+    if (box.style.display === 'flex') {
+      box.style.display = 'none';
+    } else {
+      box.style.display = 'flex';
+    }
+  };
+
+  const handleViewClick = () => {
+    const box = document.getElementById('view-dropdown');
+    if (box.style.display === 'flex') {
+      box.style.display = 'none';
+    } else {
+      box.style.display = 'flex';
+    }
+  };
 
   return (
     <div className="Filters">
@@ -39,18 +92,18 @@ function Filters() {
          <p>Hot</p>
         </div>
         <div className='country-menu'>
-          <button className='country-menu-button-visible' onClick={handleCountryClick}>United States <IoIosArrowDown /></button>
+          <button className='country-menu-button-visible' onClick={handleCountryClick}>{selectedCountry.name} <IoIosArrowDown /></button>
           <div className='country-list' id='country-list'>
             {countryList.map((countryName) => {
-              return <button id={countryName}>{countryName}</button>
+              return <button id={countryName.id} value={countryName.name} onClick={handleCountryListClick}>{countryName.name}</button>
             })}
           </div>
         </div>
-        <div className='state-menu'>
-          <button className='state-menu-button-visible' onClick={handleStateClick}>All <IoIosArrowDown /></button>
+        <div className='state-menu' id='state-menu'>
+          <button className='state-menu-button-visible' onClick={handleStateClick}>{selectedState.name} <IoIosArrowDown /></button>
           <div className='state-list' id='state-list'>
             {stateList.map((stateName) => {
-              return <button id={stateName}>{stateName}</button>
+              return <button id={stateName.id} value={stateName.name} onClick={handleStateListClick}>{stateName.name}</button>
             })}
           </div>
         </div>
@@ -63,13 +116,20 @@ function Filters() {
           <p>Top</p>
         </div>
         <div className='extra-menu'>
-          <p className='dots'>&#8226;&#8226;&#8226;</p>
+          <button className='dots' onClick={handleDotsClick}>&#8226;&#8226;&#8226;</button>
+          <div className='dots-dropdown' id='dots-dropdown'>
+            <button id='rising' value='rising'><BsArrowUpRight /> Rising</button>
+          </div>
         </div>
       </div>
       <div className='right'>
         <div className='view'>
-          <BsViewStacked />
-          <IoIosArrowDown />
+          <button className='view-button' onClick={handleViewClick}><BsViewStacked /> <IoIosArrowDown /></button>
+          <div className='view-dropdown' id='view-dropdown'>
+            <button id='card'>Card</button>
+            <button id='classic'>Classic</button>
+            <button id='compact'>Compact</button>
+          </div>
         </div>
       </div>
     </div>
