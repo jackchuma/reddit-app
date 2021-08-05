fea3c6a6cc38c08fa310b4f16b677d0cd0c84294
John Chuma <johnchuma@Johns-MacBook-Air.local>
2021-08-04 21:49:07 -0400

    New Posts page now shows posts in order

--- src/components/Posts/NewPosts.js
+++ src/components/Posts/NewPosts.js
@@ -1,11 +1,40 @@
 import React from 'react';
-//import './Posts.css';
-//import Post from '../Post/Post';
+import Post from '../Post/Post';
+import { useSelector } from 'react-redux';
+import { selectPosts } from './postsSlice';
 
 function NewPosts() {
+  const allPostsHome = useSelector(selectPosts);
+  let allPosts = Object.values(allPostsHome);
+  allPosts.sort((a,b) => b.data.created_utc - a.data.created_utc);
+
   return (
     <div className="new-posts">
-      New Posts
+      {allPosts.map(post => {
+        const id = post.data.id;
+        const author = post.data.author;
+        const thread = post.data.subreddit;
+        const title = post.data.title;
+        const ups = post.data.ups;
+        const downs = post.data.downs;
+        const created = post.data.created_utc;
+        const url = post.data.url;
+        const postHint = post.data.post_hint;
+
+        if (postHint === 'image') {
+          return <Post
+            key={id} 
+            id={id}
+            author={author}
+            thread={thread}
+            title={title}
+            ups={ups}
+            downs={downs}
+            created={created}
+            url={url} />
+        }
+        return '';
+      })}
     </div>
   );
 }
